145941e835efc7e1ca12b412dbbb4277b1794f5a
Rituj Beniwal <ritujbeniwal@gmail.com>
2017-11-09 17:31:07 +0530

    Screen Stabilization [1/3]
    Forward port to Pie by @SuperDroidBond
    
    Change-Id: Ibccb8d78d79938f42241ea3690ea66c3aace89ea
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -76,6 +76,7 @@ import com.android.server.coverage.CoverageService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.display.ColorDisplayService;
 import com.android.server.display.DisplayManagerService;
+import com.android.server.display.ScreenStabilization;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
 import com.android.server.fingerprint.FingerprintService;
@@ -1614,6 +1615,10 @@ public final class SystemServer {
                 mSystemServiceManager.startService(PocketBridgeService.class);
                 traceEnd();
             }
+
+            traceBeginAndSlog("Starting ScreenStabilization Service");
+            mSystemServiceManager.startService(ScreenStabilization.class);
+            traceEnd();
         }
 
         if (!isWatch) {
