6ef75111bd87a2d87917ac9ff066a5ea1a9317bf
SuperDroidBond <superdroidbond@yahoo.com>
2018-03-16 16:53:28 +0000

    Add Screen Stabilization Tile
    Change-Id: I852e4aca65dc7306aa1205dd7e1bced0b0780483
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -52,6 +52,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
+import com.android.systemui.qs.tiles.ScreenStabilizationTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
@@ -152,6 +153,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new FontTile(mHost);
             case "navbar":
                 return new NavBarTile(mHost);
+            case "screenstabilization":
+                return new ScreenStabilizationTile(mHost);
         }
 
         // Intent tiles.
