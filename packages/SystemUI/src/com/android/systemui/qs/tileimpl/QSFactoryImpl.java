228c65694f97e6ea1e9f4f7d88678e40fe1460f2
romanbb <romanbirg@gmail.com>
2018-08-23 08:59:35 +0200

    Add Compass tile
    - Port to Oreo - LorDClockAN
    - Port to Pie - ZeNiXxX
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
@@ -164,6 +165,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new LocaleTile(mHost);
             case "cpuinfo":
                 return new CPUInfoTile(mHost);
+            case "compass":
+                return new CompassTile(mHost);
         }
 
         // Intent tiles.
