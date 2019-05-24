109df4b90efd33fe43b87630ed4c76fdf036e6a6
AnkitGourav <Ankitgourav035@gmail.com>
2019-05-24 21:04:49 +0530

    Battery Estimates options [1/2]
    * Battery Estimate Toggle
    * Battery Estimates position option
    * Fix estimate layout not being refresh when changing battery percent style
    
    Change-Id: I3bda062c76efeb962cf85ffcc272db12296e78db
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -17,6 +17,7 @@ package com.android.systemui;
 
 import static android.app.StatusBarManager.DISABLE2_SYSTEM_ICONS;
 import static android.app.StatusBarManager.DISABLE_NONE;
+import static android.provider.Settings.System.SHOW_BATTERY_ESTIMATE;
 import static android.provider.Settings.System.SHOW_BATTERY_PERCENT;
 import static android.provider.Settings.Secure.STATUS_BAR_BATTERY_STYLE;
 
@@ -236,6 +237,9 @@ public class BatteryMeterView extends LinearLayout implements
         getContext().getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(SHOW_BATTERY_PERCENT), false, mSettingObserver, mUser);
         updateShowPercent();
+        getContext().getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(SHOW_BATTERY_ESTIMATE), false, mSettingObserver, mUser);
+        updatePercentText();
         Dependency.get(TunerService.class).addTunable(this, STATUS_BAR_BATTERY_STYLE);
         Dependency.get(ConfigurationController.class).addCallback(this);
         mUserTracker.startTracking();
@@ -309,7 +313,29 @@ public class BatteryMeterView extends LinearLayout implements
 
     private void onEstimateFetchComplete(String estimate) {
         if (estimate != null) {
-            mBatteryPercentView.setText(estimate);
+            int percentageStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                    SHOW_BATTERY_PERCENT, 0, mUser);
+            if (getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT) {
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.SHOW_BATTERY_ESTIMATE, 0) != 0) {
+                    mBatteryPercentView.setText(NumberFormat.getPercentInstance().format(mLevel / 100f) + " | " + estimate);
+                } else {
+                    setPercentTextAtCurrentLevel();
+                }
+            } else {
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.SHOW_BATTERY_ESTIMATE, 0) != 0) {
+                    if ( percentageStyle == 1 ) {
+                        mBatteryPercentView.setText(NumberFormat.getPercentInstance().format(mLevel / 100f) + " | " + estimate);
+                    } else if (percentageStyle == 0 ) {
+                        mBatteryPercentView.setText(estimate);
+                    } else {
+                        setPercentTextAtCurrentLevel();
+                    }
+                } else {
+                    setPercentTextAtCurrentLevel();
+                }
+            }
         } else {
             setPercentTextAtCurrentLevel();
         }
@@ -376,6 +402,8 @@ public class BatteryMeterView extends LinearLayout implements
                     getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? 0 : startPadding,
                     0, 0, 0);
         }
+
+        updatePercentText();
     }
 
     private void createPercentView() {
