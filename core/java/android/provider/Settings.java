da048f067230d22b95e8b38556932e3d6a006c83
AnkitGourav <Ankitgourav035@gmail.com>
2019-06-14 15:15:57 +0530

    Powermenu Background filter [1/2]
    initial idea of @Descendant
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5649,6 +5649,17 @@ public final class Settings {
          */
         public static final String WEATHER_LOCKSCREEN_ENABLED = "weather_lockscreen_enabled";
 
+        /**
+        * Powermenu Background
+        * @hide
+        */
+        public static final String POWER_MENU_BG = "power_menu_bg";
+
+        /**
+        * Powermenu Background Style
+        * @hide
+        */
+        public static final String POWER_MENU_BG_STYLE = "power_menu_bg_style";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
