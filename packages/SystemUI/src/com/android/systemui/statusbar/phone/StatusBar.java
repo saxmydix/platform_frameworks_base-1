2d7e0973c20d6035a900032267e3152b348f3547
AnkitGourav <Ankitgourav035@gmail.com>
2019-06-15 12:46:41 +0530

    Accent Tinted and blurred filter for media coverart [1/2]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1929,6 +1929,11 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                     case 4:
                         artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getGrayscaleBlurredImage(mContext, artworkBitmap, 25.0f));
                         break;
+                    case 5:
+                        Drawable aww = new BitmapDrawable(mBackdropBack.getResources(), artworkBitmap);
+                        Bitmap aaww = ImageHelper.getColoredBitmap(aww, mContext.getResources().getColor(R.color.sammy_minutes_accent));
+                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getBlurredImage(mContext, aaww, 7.0f));
+                        break;
                     case 0:
                     default:
                         artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), artworkBitmap);
