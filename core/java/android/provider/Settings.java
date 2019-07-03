235db4086b46fd7a9c9c5869c24c0daa22c15cc8
Max <xyyx@mail.ru>
2019-07-03 11:57:30 +0000

    Unlock keystore with fingerprint after reboot (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5585,6 +5585,12 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
