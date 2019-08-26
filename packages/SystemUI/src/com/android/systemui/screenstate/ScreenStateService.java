b60c5282d6a704e1c6f505f26dd847f1f5498314
saxmydix <linexsg@gmail.com>
2019-08-26 20:06:32 +0800

    Update ScreenStateService.java

--- packages/SystemUI/src/com/android/systemui/screenstate/ScreenStateService.java
+++ packages/SystemUI/src/com/android/systemui/screenstate/ScreenStateService.java
@@ -36,7 +36,6 @@ public class ScreenStateService extends Service  {
     private static final String TAG = "ScreenStateService";
     private BroadcastReceiver mPowerKeyReceiver;
     private TwoGToggle mTwoGToggle;
-    private ThreeGToggle mThreeGToggle;
     private GpsToggle mGpsToggle;
     private MobileDataToggle mMobileDataToggle;
     private boolean mEnabled = true;
@@ -109,8 +108,6 @@ public class ScreenStateService extends Service  {
         fAllToggles = new ArrayList<ScreenStateToggle>();
         mTwoGToggle = new TwoGToggle(mContext);
         fAllToggles.add(mTwoGToggle);
-        mThreeGToggle = new ThreeGToggle(mContext);
-        fAllToggles.add(mThreeGToggle);
         mGpsToggle = new GpsToggle(mContext);
         fAllToggles.add(mGpsToggle);
         mMobileDataToggle = new MobileDataToggle(mContext);
